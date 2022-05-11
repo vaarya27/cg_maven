abc170537848af6b3d2c9845d657b7033f104033
Arya <vaishnavi.arya@capgemini.com>
2022-05-11 17:44:40 +0530

    first

---
+++ src/main/java/com/cg/App.java
@@ -0,0 +1,17 @@
+package com.cg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public int add(int a, int b) {
+    	return a+b;
+    }
+}
---
+++ src/test/java/com/cg/AppTest.java
@@ -0,0 +1,26 @@
+package com.cg;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void testAdd() {
+    	App app = new App();
+    	assertEquals(19, app.add(9,  10));
+    }
+}
